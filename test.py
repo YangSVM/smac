fix the wrong dim reward in corner cases

test.py
@@ -6,12 +6,12 @@ env.reset()
 
 np.random.seed(0)
 
-for i in range(20):
+for i in range(200):
 
     actions = []
     for i in range(3):
         action_avail = env.get_avail_agent_actions(i)
-        print('agent ', i, 'avail_action: ', action_avail)
+        # print('agent ', i, 'avail_action: ', action_avail)
         if action_avail[0] == 1:
             # 如果死亡，只有一个动作
             action = 0
@@ -19,18 +19,25 @@ for i in range(20):
             # 如果能够攻击，就攻击
             avail_actions_ind = np.nonzero(action_avail[6:9])[0]
             action = np.random.choice(avail_actions_ind)+6
-        else:
+            print('attack')
+        elif action_avail[4] == 1:
             # 往东走
             action = 4
+        else:
+            avail_actions_ind = np.nonzero(action_avail)[0]
+            action = np.random.choice(avail_actions_ind)
+
         actions.append(action)
-    print('actions: ', actions)
+    # print('actions: ', actions)
     
-    r,i,l =env.step(actions)
+    r,t,info =env.step(actions)
+    print(info)
     r_totall  = env.reward_battle()
     r_multi = env.reward_multi_task()
     r_multi_sum = 0
     for ri in r_multi:
         r_multi_sum +=ri
-    if r_multi_sum  != r_totall:
-        print('error')
+    if r_multi_sum  != r_totall or len(r_multi)!=3:
+        print('error','*'*60)
+
     print('reward: ', r)