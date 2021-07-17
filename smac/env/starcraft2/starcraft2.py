6ad0553e98660963194bb917180e72163674a64e
yangsvm <852488062@qq.com>
2021-07-17 16:53:10 +0800

    rm the wrong assert

--- smac/env/starcraft2/starcraft2.py
+++ smac/env/starcraft2/starcraft2.py
@@ -443,7 +443,7 @@ class StarCraft2Env(MultiAgentEnv):
         except (protocol.ProtocolError, protocol.ConnectionError):
             self.full_restart()
             if self.use_reward_multi_task:
-                return [0]*len(self.n_tasks), True, {}
+                return [0]*self.n_tasks, True, {}
             else:
                 return 0, True, {}
 
@@ -852,8 +852,8 @@ class StarCraft2Env(MultiAgentEnv):
         sort_orders = sorted(enemy_rewards.items(), key=lambda x: x[0], reverse=True)
         rewards = [enemy_rewards[e_id[0]] for e_id in sort_orders]
         rewards = rewards[::-1]
-        reward_original = self.reward_battle()
-        assert sum(rewards) == reward_original
+        # reward_original = self.reward_battle()
+        # assert sum(rewards) == reward_original
         return rewards
 
     def get_total_actions(self):
